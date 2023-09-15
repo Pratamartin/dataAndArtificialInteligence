9a2ed6a784282b06a2bfbb3217a0810b6a00c0ae
Martinho Prata <martinhoprata95@gmail.com>
2023-09-15 06:03:27 -0300

    Start

---
+++ IFoodDevWeek.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# getting the folder file
+file = 'D:\\Python - Projects\\dataScienceAndArtificialIntelligence\\csv\\feedbacks.csv'
+
+# reading CSV file
+dados = pd.read_csv(file, delimiter=';')
+
